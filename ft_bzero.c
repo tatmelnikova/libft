3798c53f253de022bb8e812ae5f119b2a6da5f84
Tatiana Kazmina <tkazmina@student.codam.nl>
2026-03-11 15:38:40 +0100

    ft_bzero

---
+++ ft_bzero.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkazmina <tkazmina@student.codam.nl>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/11 15:32:48 by tkazmina          #+#    #+#             */
+/*   Updated: 2026/03/11 15:38:09 by tkazmina         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+// The bzero() function erases the data in the n bytes of the memory
+// starting at the location pointed to by s, by writing zeros
+// (bytes containing '\0') to that area.
+void	bzero(void *b, size_t n)
+{
+	char	*p;
+
+	while (n > 0)
+	{
+		*p = '\0';
+		n--;
+		b++;
+	}
+}
