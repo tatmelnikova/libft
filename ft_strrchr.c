2062673418f01ac3f8d55d390706044dcd36c5e4
Tatiana Kazmina <tkazmina@student.codam.nl>
2026-03-12 14:25:06 +0100

    ft_strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,35 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tkazmina <tkazmina@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/12 14:13:24 by tkazmina          #+#    #+#             */
-/*   Updated: 2026/03/12 14:24:27 by tkazmina         ###   ########.fr       */
+/*   Created: 2026/03/12 14:19:31 by tkazmina          #+#    #+#             */
+/*   Updated: 2026/03/12 14:24:44 by tkazmina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
-// The strchr() function returns a pointer to the first occurrence 
+// The strrchr() function returns a pointer to the last occurrence
 // of the character c in the string s.
-// The strchr() function returns a pointer to the matched character
-// or NULL if the character is not found.  The terminating null
-// byte is considered part of the string, so that if c is
-// specified as '\0', these functions return a pointer to the terminator.
-char	*strchr(const char *s, int c)
+// The strrchr() function returns a pointer to the
+// matched character or NULL if the character is not found.
+// The terminating null byte is considered part of the string, so
+// that if c is specified as '\0', these functions return a 
+// pointer to the terminator.
+char	*strrchr(const char *s, int c)
 {
-	int	i;
+	int		i;
+	char	*last_match;
 
 	i = 0;
 	while (s[i])
 	{
 		if (s[i] == c)
-			return (s[i]);
+			last_match = s[i];
 		i++;
 	}
 	if (s[i] == '\0' && c == '\0')
-		return (s[i]);
+		last_match = s[i];
+	if (last_match)
+		return (last_match);
 	return (NULL);
 }
