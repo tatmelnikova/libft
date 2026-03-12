fb689345dadbcbd2f2e33c3dbbb4905fa09d198b
Tatiana Kazmina <tkazmina@student.codam.nl>
2026-03-12 17:12:43 +0100

    ft_strlen doc

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,10 +6,12 @@
 /*   By: tkazmina <tkazmina@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/23 17:18:38 by tkazmina          #+#    #+#             */
-/*   Updated: 2026/01/23 17:22:29 by tkazmina         ###   ########.fr       */
+/*   Updated: 2026/03/12 17:12:16 by tkazmina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// The strlen() function calculates the length of the string pointed
+// to by s, excluding the terminating null byte ('\0').
 int	ft_strlen(char *str)
 {
 	char	c;
