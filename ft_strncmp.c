2cc315eff571e1d10cc2f69c32ab99755561ac64
Tatiana Kazmina <tkazmina@student.codam.nl>
2026-03-16 18:07:07 +0100

    added cast to unsigned char for ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: tkazmina <tkazmina@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/25 18:59:52 by tkazmina          #+#    #+#             */
-/*   Updated: 2026/03/12 14:34:40 by tkazmina         ###   ########.fr       */
+/*   Updated: 2026/03/16 18:05:47 by tkazmina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,8 +35,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		if ((unsigned char)s1[i] != (unsigned char)s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
 	return (0);
