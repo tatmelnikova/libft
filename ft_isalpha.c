7f1eb8e9d32212d7b1903394075f9ae93ce20922
Tatiana Kazmina <tkazmina@student.codam.nl>
2026-03-17 18:19:43 +0100

    fix ft_isalpha for numbers greater than char

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,50 +6,29 @@
 /*   By: tkazmina <tkazmina@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/24 16:47:21 by tkazmina          #+#    #+#             */
-/*   Updated: 2026/03/16 17:35:11 by tkazmina         ###   ########.fr       */
+/*   Updated: 2026/03/17 17:34:59 by tkazmina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-static int	is_lowercase(char c)
-{
-	int	result;
-
-	result = 0;
-	if (c >= 'a' && c <= 'z')
-	{
-		result = 1;
-	}
-	return (result);
-}
-
-static int	is_uppercase(char c)
-{
-	int	result;
-
-	result = 0;
-	if (c >= 'A' && c <= 'Z')
-	{
-		result = 1;
-	}
-	return (result);
-}
-
 int	ft_isalpha(int c)
 {
-	return (is_lowercase(c) || is_uppercase(c));
+	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
 }
 
 // #include <stdio.h>
-
+// #include <ctype.h>
 // int main(void)
 // {
-// 	int	result;
+// 	int	result, result1;
+// 	for (int i = -1; i < 530; i++)
+// 	{
+// 		result = ft_isalpha(i);
+// 		result1 = isalpha(i);
+// 		if (!!result1 != !!result)
+// 		{
+// 			return 1;
+// 		}
+// 	}
 
-// 	result = ft_isalpha((int)'a');
-// 	printf("result = %d\n", result);
-// 	result = ft_isalpha(0);
-// 	printf("result = %d\n", result);
-// 	result = ft_isalpha(-9);
-// 	printf("result = %d\n", result);
-// 	return (result);
+// 	return (0);
 // }
