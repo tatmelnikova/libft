5bf23450ad28e0997ef223e77d4c179897e51758
Tatiana Kazmina <tkazmina@student.codam.nl>
2026-03-19 16:58:29 +0100

    ft_strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkazmina <tkazmina@student.codam.nl>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/19 14:47:04 by tkazmina          #+#    #+#             */
+/*   Updated: 2026/03/19 14:47:04 by tkazmina         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <string.h>
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*result;
+	char	*r;
+
+	if (!s1 || !s2)
+		return (NULL);
+	result = malloc(sizeof(char) * (ft_strlen((char *)s1)
+				+ ft_strlen((char *)s2) + 1));
+	if (!result)
+		return (NULL);
+	r = result;
+	while (*s1)
+		*r++ = *s1++;
+	while (*s2)
+		*r++ = *s2++;
+	*r = '\0';
+	return (result);
+}
