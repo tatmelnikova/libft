5744b67699b5019a85777c65f3dbefd44b457d2d
Tatiana Kazmina <tkazmina@student.codam.nl>
2026-03-23 14:54:54 +0100

    ft_striteri

---
+++ ft_striteri.c
@@ -0,0 +1,35 @@
+#include "libft.h"
+
+// Applies the function ’f’ on each character of the string passed
+// as argument, and passing its index as first argument. Each
+// character is passed by address to ’f’ to be modified if necessary
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	size_t	i;
+
+	if (!s || !f)
+		return ;
+	i = 0;
+	while (i < ft_strlen(s))
+	{
+		(*f)(i, &s[i]);
+		i++;
+	}
+}
+
+/*
+void map_fn(unsigned int i, char *c)
+{
+	*c = *c + i;
+}
+
+int main(void)
+{
+	char str[] = "aaaaaa";
+	void (*f)(unsigned int, char*) = map_fn;
+
+	ft_striteri(str, f);
+	printf("%s\n", str);
+	return 0;
+}
+*/
