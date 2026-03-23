804901b3beb8e7d18a3ea80a4fb0c8fe614a8d96
Tatiana Kazmina <tkazmina@student.codam.nl>
2026-03-23 15:08:10 +0100

    new functions added to Makefile and .h file

--- libft.h
+++ libft.h
@@ -46,5 +46,10 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putendl_fd(char *s, int fd);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 
 #endif
