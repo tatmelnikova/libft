428a8d3aebdb25699cbda241d76fe9a64555ba6e
Tatiana Kazmina <tkazmina@student.codam.nl>
2026-03-23 15:00:30 +0100

    ft_putstr_fd.c

---
+++ ft_putstr_fd.c
@@ -0,0 +1,15 @@
+#include "libft.h"
+
+// Outputs the string ’s’ to the given file descriptor
+void	ft_putstr_fd(char *s, int fd)
+{
+	if (!s || fd < 0)
+		return ;
+	write(fd, s, ft_strlen(s));
+}
+
+// int main(void)
+// {
+// 	ft_putstr_fd("aaa bbb\n", 1);
+// 	return 0;
+// }
